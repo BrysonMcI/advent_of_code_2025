807d54062ef5c351a29f776833f3d0f460400e0f
Bryson McIver <brysonmciver@gmail.com>
2025-12-03 20:39:23 -0600

    Part 2, unoptimized

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+const DIGIT_SIZE: usize = 12;
+
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
     println!(
@@ -5,15 +7,31 @@ fn main() {
         input
             .lines()
             .map(|line| {
-                let highest_char = line
-                    .char_indices()
-                    .rev()
-                    .skip(1)
-                    .max_by(|a, b| a.1.cmp(&b.1))
-                    .unwrap();
-                let next_highest = line.chars().skip(highest_char.0 + 1).max().unwrap();
-                highest_char.1.to_digit(10).unwrap() * 10 + next_highest.to_digit(10).unwrap()
+                let mut highest_chars: [u64; DIGIT_SIZE] = [0; DIGIT_SIZE];
+                let mut digits_to_find = DIGIT_SIZE;
+                let mut last_highest_char_index = 0; // bad name, this is index after
+                while digits_to_find > 0 {
+                    let next = line
+                        .char_indices()
+                        .skip(last_highest_char_index)
+                        .collect::<Vec<_>>()
+                        .into_iter()
+                        .rev()
+                        .skip(digits_to_find - 1)
+                        .max_by(|a, b| a.1.cmp(&b.1))
+                        .unwrap();
+                    highest_chars[DIGIT_SIZE - digits_to_find] =
+                        next.1.to_digit(10).unwrap() as u64;
+                    last_highest_char_index = next.0 + 1;
+                    digits_to_find -= 1;
+                }
+
+                let mut sum: u64 = 0;
+                for (i, digit) in highest_chars.iter().enumerate() {
+                    sum += digit * (10_u64.pow((DIGIT_SIZE - 1 - i) as u32));
+                }
+                sum
             })
-            .sum::<u32>()
+            .sum::<u64>()
     );
 }
