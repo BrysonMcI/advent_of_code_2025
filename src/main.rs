593b44035eb5f675c800781ec908c16ee70eaaa3
Bryson McIver <brysonmciver@gmail.com>
2025-12-03 21:20:27 -0600

    Some simplification

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,15 @@ fn main() {
             .lines()
             .map(|line| {
                 let mut sum: u64 = 0;
-                let mut digits_to_find = DIGIT_SIZE;
-                let mut last_highest_char_index = 0; // bad name, this is index after
-                while digits_to_find > 0 {
-                    let search_end = line.len() - digits_to_find + 1;
-                    let next = line
+                let mut last_highest_char_index = 0;
+                for remaining in (1..=DIGIT_SIZE).rev() {
+                    let search_end = line.len() - remaining + 1;
+                    let next = line[last_highest_char_index..search_end] // skip to our last highest number and ignore enough numbers to fill the rest of the digits
                         .char_indices()
-                        .skip(last_highest_char_index) // skip to our last highest number
-                        .take_while(|(idx, _)| *idx < search_end) // go search for the next one that leaves us enough remaining digits
                         .max_by(|a, b| a.1.cmp(&b.1).then_with(|| b.0.cmp(&a.0))) // take the highest next one, but closest to the front
                         .unwrap();
                     sum = (sum * 10) + next.1.to_digit(10).unwrap() as u64;
-                    last_highest_char_index = next.0 + 1;
-                    digits_to_find -= 1;
+                    last_highest_char_index += next.0 + 1;
                 }
                 sum
             })
