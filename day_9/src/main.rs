a78b90c4abb22d56d3fc084a69793f567e8851f4
Bryson McIver <brysonmciver@gmail.com>
2025-12-09 23:27:17 -0600

    Day 9 Part 2 but it doesn't work

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -10,12 +10,26 @@ fn main() {
         })
         .collect();
 
-    let biggest_area = cords
-        .iter()
-        .combinations(2)
-        .map(|pair| ((pair[1].0 - pair[0].0).abs() + 1) * ((pair[1].1 - pair[0].1).abs() + 1))
-        .max()
-        .unwrap();
+    let potential_areas = cords.iter().combinations(2).sorted_by(|b, a| {
+        (((a[1].0 - a[0].0).abs() + 1) * ((a[1].1 - a[0].1).abs() + 1))
+            .cmp(&(((b[1].0 - b[0].0).abs() + 1) * ((b[1].1 - b[0].1).abs() + 1)))
+    });
 
-    println!("{:?}", biggest_area);
+    println!("{:?}", cords);
+    println!("{:?}", potential_areas);
+    for pair in potential_areas {
+        if cords.iter().any(|cord| {
+            ((cord.0 > pair[0].0 && cord.0 < pair[1].0)
+                || (cord.0 > pair[1].0 && cord.0 < pair[0].0))
+                && ((cord.1 > pair[0].1 && cord.1 < pair[1].1)
+                    || (cord.1 > pair[1].1 && cord.1 < pair[0].1))
+        }) {
+            continue;
+        }
+        println!(
+            "{:?}",
+            (((pair[1].0 - pair[0].0).abs() + 1) * ((pair[1].1 - pair[0].1).abs() + 1))
+        );
+        return;
+    }
 }
