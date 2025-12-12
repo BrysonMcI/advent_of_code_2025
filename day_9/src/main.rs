6d58e25022592f6e4144cb14bfc747b410516e26
Bryson McIver <brysonmciver@gmail.com>
2025-12-12 11:01:41 -0600

    Day 9 Part 2 Add Perimeters

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -1,7 +1,7 @@
 use itertools::Itertools;
 
 fn main() {
-    let input = std::fs::read_to_string("example.txt").unwrap();
+    let input = std::fs::read_to_string("input.txt").unwrap();
     let cords: Vec<(i64, i64)> = input
         .lines()
         .map(|l| {
@@ -15,10 +15,20 @@ fn main() {
             .cmp(&(((b[1].0 - b[0].0).abs() + 1) * ((b[1].1 - b[0].1).abs() + 1)))
     });
 
-    println!("{:?}", cords);
-    println!("{:?}", potential_areas);
+    let blocker_cords: Vec<(i64, i64)> = cords.iter().circular_tuple_windows().flat_map(|(first, next)| match first.0 == next.0 {
+        true => match first.1 < next.1 {
+            true => (first.1..=next.1).map(|y| (first.0, y)).collect::<Vec<(i64, i64)>>(),
+            false => (next.1..=first.1).map(|y| (first.0, y)).collect::<Vec<(i64, i64)>>(),
+        }
+        false => match first.0 < next.0 {
+            true => (first.0..=next.0).map(|x| (x, first.1)).collect::<Vec<(i64, i64)>>(),
+            false => (next.0..=first.0).map(|x| (x, first.1)).collect::<Vec<(i64, i64)>>(),
+        }
+    }).collect();
+
+    
     for pair in potential_areas {
-        if cords.iter().any(|cord| {
+        if blocker_cords.iter().any(|cord| {
             ((cord.0 > pair[0].0 && cord.0 < pair[1].0)
                 || (cord.0 > pair[1].0 && cord.0 < pair[0].0))
                 && ((cord.1 > pair[0].1 && cord.1 < pair[1].1)
