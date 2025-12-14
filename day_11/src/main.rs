0e65d71008705b988f16060e16eff95c12e7f988
Bryson McIver <brysonmciver@gmail.com>
2025-12-13 19:31:22 -0600

    Day 11 Part 2

--- day_11/src/main.rs
+++ day_11/src/main.rs
@@ -1,8 +1,10 @@
 use pathfinding::prelude::count_paths;
 use std::collections::HashMap;
 
-const START_NODE: &str = "you";
+const START_NODE: &str = "svr";
 const END_NODE: &str = "out";
+const FFT_NODE: &str = "fft";
+const DAC_NODE: &str = "dac";
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
@@ -15,11 +17,69 @@ fn main() {
         graph.insert(node, connections);
     }
 
-    println!("{:?}", graph);
-    let paths = count_paths(
+    let start_to_fft_no_dac = count_paths(
         START_NODE.to_string(),
+        |n: &String| {
+            graph
+                .get(n)
+                .unwrap_or(&empty)
+                .iter()
+                .filter(|a| *a != DAC_NODE)
+                .cloned()
+        },
+        |n: &String| *n == FFT_NODE,
+    );
+    let fft_to_dac = count_paths(
+        FFT_NODE.to_string(),
         |n: &String| graph.get(n).unwrap_or(&empty).iter().cloned(),
+        |n: &String| *n == DAC_NODE,
+    );
+    let dac_to_end_no_fft = count_paths(
+        DAC_NODE.to_string(),
+        |n: &String| {
+            graph
+                .get(n)
+                .unwrap_or(&empty)
+                .iter()
+                .filter(|a| *a != FFT_NODE)
+                .cloned()
+        },
         |n: &String| *n == END_NODE,
     );
-    println!("{:?}", paths);
+
+    let start_to_dac_no_fft = count_paths(
+        START_NODE.to_string(),
+        |n: &String| {
+            graph
+                .get(n)
+                .unwrap_or(&empty)
+                .iter()
+                .filter(|a| *a != FFT_NODE)
+                .cloned()
+        },
+        |n: &String| *n == DAC_NODE,
+    );
+    let dac_to_fft = count_paths(
+        DAC_NODE.to_string(),
+        |n: &String| graph.get(n).unwrap_or(&empty).iter().cloned(),
+        |n: &String| *n == FFT_NODE,
+    );
+    let fft_to_end_no_dac = count_paths(
+        FFT_NODE.to_string(),
+        |n: &String| {
+            graph
+                .get(n)
+                .unwrap_or(&empty)
+                .iter()
+                .filter(|a| *a != DAC_NODE)
+                .cloned()
+        },
+        |n: &String| *n == END_NODE,
+    );
+
+    println!(
+        "{:?}",
+        (start_to_fft_no_dac * fft_to_dac * dac_to_end_no_fft)
+            + (start_to_dac_no_fft * dac_to_fft * fft_to_end_no_dac)
+    );
 }
